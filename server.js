3302e2a14e69577894921c0bec03932486848b63
prathmesh36 <prathmesh36@yahoo.com>
2018-12-27 10:33:24 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.use(express.static(__dirname+'/public'));
+app.set('view engine', 'hbs');
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log=`${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFileSync('server.log',log+'\n');
+    next();
+});
+
+app.use((req,res,next)=>{
+    res.render('maintainance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('titlePresentation',(text)=>{
+    return text.charAt(0).toUpperCase() + text.slice(1);
+})
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMessage:'welcome to my blog'
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page'
+    });
+
+});
+
+app.get('/errorpage',(req,res)=>{
+    res.send({
+        ErrorMessage:"Access Denied: Page Under Construction"
+    });
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000');
+});
